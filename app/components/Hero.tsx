18788a602a5c64a59f4a9e5a71d22c311f242090
Mary <gretchen7887@gmail.com>
2024-03-04 21:39:29 +0400

    Upd

---
+++ app/components/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book or rent a car — quickly and easily!
+        </h1>
+
+        <p className="hero__subtitle">
+          Stremline your car rental experience with our effortless booking
+          process.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
+import { Hero } from './components'
+
 
 export default function Home() {
-  return <main className="overflow-hidden"></main>
+  return <main className="overflow-hidden">
+    <Hero/>
+  </main>
 }
+  
