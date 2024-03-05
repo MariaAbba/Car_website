b90101711c1713c233b7db457751775b4fcfbef9
Mary <gretchen7887@gmail.com>
2024-03-05 21:39:34 +0400

    Upd

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import { Hero } from '@/components'
+import CustomFilter from '@/components/CustomFilter'
+import SearchBar from '@/components/SearchBar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width id='discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="fuel" />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import {footerLinks} from '@/constants'
+import { footerLinks } from '@/constants'
 
 const Footer = () => {
   return (
@@ -23,9 +22,33 @@ const Footer = () => {
         </div>
 
         <div className="footer__links">
-{footerLinks.map((link) => (
-  <div key={link.title} className='footer__link'></div>
-))}
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2024 CarHub. All Rights Reserved.</p>
+
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
         </div>
       </div>
     </footer>
