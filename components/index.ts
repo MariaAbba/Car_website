14e5153d4154d608eca6236c30913e64f63ace86
Mary <gretchen7887@gmail.com>
2024-03-07 19:23:11 +0400

    Upd layout

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ import Hero from '../components/Hero'
 import CustomButton from '../components/CustomButton'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import SearchBar from '../components/SearchBar'
+import CustomFilter from '../components/CustomFilter'
 
-export { Hero, CustomButton, Navbar, Footer }
+export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter }
