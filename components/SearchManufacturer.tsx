2d663d940b1b5c3bb45cfa3856b7522195843a62
Mary <gretchen7887@gmail.com>
2024-03-09 14:24:57 +0400

    Upd layout

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufacturerProps } from '@/types'
 import React from 'react'
 
@@ -5,7 +7,13 @@ const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>
+  return <div className='search-manufacturer'>
+    <Combobox>
+      <div className="relative w-full">
+
+      </div>
+    </Combobox>
+  </div>
 }
 
 export default SearchManufacturer
