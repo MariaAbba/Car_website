303c1cf4b77cf09ea9a44c2fef80650afa48b11b
Mary <gretchen7887@gmail.com>
2024-03-10 11:45:45 +0400

    Upd

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -25,7 +25,7 @@ const SearchManufacturer = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
