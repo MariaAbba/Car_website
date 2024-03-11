a6ff3f47cd8725e5b3ad26775e230cd68c28954a
Mary <gretchen7887@gmail.com>
2024-03-11 20:05:20 +0400

    Upd

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 
 import { CarProps } from '@/types'
 import CustomButton from './CustomButton'
+import { calculateCarRent } from '@/utils'
 
 interface CarCardProps {
   car: CarProps
@@ -11,6 +12,9 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, drive, make, model, transmission, year } = car
+
+  const carRent = calculateCarRent(city_mpg, year)
+
   return (
     <div className="car-card-group">
       <div className="car-card__content">
@@ -19,18 +23,61 @@ const CarCard = ({ car }: CarCardProps) => {
         </h2>
       </div>
 
-      <p>
-        <span>Car Rent...</span>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px font-medium">/day</span>
       </p>
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          alt="car model"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image 
+            src='/steering-wheel.svg'
+            width={20}
+            height={20}
+            alt='steering wheel'
+            />
+            <p className='text-[14px]'>
+              {transmission === 'a' ? 'Automatic' : 'Manual'}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image 
+            src='/tire.svg'
+            width={20}
+            height={20}
+            alt='tyre'
+            />
+            <p className='text-[14px]'>
+              {drive.toUpperCase()}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image 
+            src='/steering-wheel.svg'
+            width={20}
+            height={20}
+            alt='steering wheel'
+            />
+            <p className='text-[14px]'>
+              {transmission === 'a' ? 'Automatic' : 'Manual'}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default CarCard
-
-// class,
-// combination_mp,
-// cylinder,
-// displacement,
-// fuel_type,
-// highway_mp,
