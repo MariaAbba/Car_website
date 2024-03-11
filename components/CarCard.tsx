a3ae2894ebb1f2ba9642fb4c4f9be3ad64ca822b
Mary <gretchen7887@gmail.com>
2024-03-11 18:33:01 +0400

    Upd

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,10 +1,24 @@
 'use client'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
 
-const CarCard = () => {
-  return (
-    <div>CarCard</div>
-  )
+import { CarProps } from '@/types'
+import CustomButton from './CustomButton'
+
+interface CarCardProps {
+  car: CarProps
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, drive, make, model, transmission, year } = car
+  return <div>CarCard</div>
 }
 
 export default CarCard
+
+// class,
+// combination_mp,
+// cylinder,
+// displacement,
+// fuel_type,
+// highway_mp,
--- types/index.ts
+++ types/index.ts
@@ -3,11 +3,25 @@ import { MouseEventHandler } from 'react'
 export interface CustomButtonProps {
   title: string
   containerStyles?: string
-  btnType?:'button' | 'submit' 
-  handledClick?: 
-  MouseEventHandler<HTMLButtonElement>
+  btnType?: 'button' | 'submit'
+  handledClick?: MouseEventHandler<HTMLButtonElement>
 }
 export interface SearchManufacturerProps {
   manufacturer: string
   setManufacturer: (manufacturer: string) => void
+}
+
+export interface CarCardProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
