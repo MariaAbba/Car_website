eb26338e47c235702beee91fe44627b73e8a8c04
Mary <gretchen7887@gmail.com>
2024-03-19 17:43:29 +0400

    Upd

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -29,6 +29,40 @@ const CardDetails = ({ isOpen, closeModal, car }: carDetailsProps) => {
           >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all gap-5 flex flex-col">
+                  <button
+                    type="button"
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                    onClick={closeModal}
+                  >
+                    <Image
+                      src="/close.svg"
+                      alt="close"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    />
+                  </button>
+
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg"></div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
         </Dialog>
       </Transition>
     </>
