1c1f1bd79c80af3823260983bf9ecf86c2d3f01b
Mary <gretchen7887@gmail.com>
2024-03-19 15:40:52 +0400

    Upd

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -18,7 +18,15 @@ const CardDetails = ({ isOpen, closeModal, car }: carDetailsProps) => {
     <>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
         </Dialog>
