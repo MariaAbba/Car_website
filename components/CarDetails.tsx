8b90dd81a0adf589ecdee4783b4290d76915315f
Mary <gretchen7887@gmail.com>
2024-03-19 21:03:44 +0400

    Upd

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -57,7 +57,15 @@ const CardDetails = ({ isOpen, closeModal, car }: carDetailsProps) => {
                   </button>
 
                   <div className="flex-1 flex flex-col gap-3">
-                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg"></div>
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        src="/hero.png"
+                        alt="car model"
+                        fill
+                        priority
+                        className="object-contain"
+                      />
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
