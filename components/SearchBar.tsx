b1b72f2b1ba354d92401cd6d581e65fe7aef2ca0
Mary <gretchen7887@gmail.com>
2024-03-19 22:50:57 +0400

    Upd

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,19 @@
 
 import React from 'react'
 import { SearchManufacturer } from '.'
+import Image from 'next/image'
+
+const SearchButton = ({otherClasses} : {otherClasses: string}) => {
+  ;<button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+}
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = React.useState('')
@@ -13,6 +26,8 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+
+        <SearchButton otherClasses="sm:hidden" />
       </div>
     </form>
   )
