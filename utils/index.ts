d8bb3b50bd21bac6134b53521295151d2fc3df4c
Mary <gretchen7887@gmail.com>
2024-03-19 22:45:47 +0400

    Upd

--- utils/index.ts
+++ utils/index.ts
@@ -7,7 +7,7 @@ export async function fetchCars() {
   }
 
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',
     {
       headers: headers,
     }
