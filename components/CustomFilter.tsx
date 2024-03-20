2d2faa875ec88bccb8f68d9d345f8556ef482add
Mary <gretchen7887@gmail.com>
2024-03-20 21:22:13 +0400

    Upd

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,13 +5,27 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { Listbox, Transition } from '@headlessui/react'
 import { CustomFilterProps } from '@/types'
+import { updateSearchParams } from '@/utils'
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter()
   const [selected, setSelected] = React.useState(options[0])
+
+  const handleUpdateParams = (e: { type: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase())
+
+    router.push(newPathName)
+  }
 
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e)
+          handleUpdateParams(e)
+        }}
+      >
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
@@ -34,11 +48,21 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
                 <Listbox.Option
                   key={option.title}
                   value={option}
-                  className={({active}) => `relative cursor-default py-2 px-4 select-none ${
-                    active ? 'bg-primary-blue text-white' : 'text-gray-900'
-                  }`}
+                  className={({ active }) =>
+                    `relative cursor-default py-2 px-4 select-none ${
+                      active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                    }`
+                  }
                 >
-                  {(selected) => <span>{option.title}</span>}
+                  {(selected) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? 'font-medium' : 'font-normal'
+                      }`}
+                    >
+                      {option.title}
+                    </span>
+                  )}
                 </Listbox.Option>
               ))}
             </Listbox.Options>
--- utils/index.ts
+++ utils/index.ts
@@ -45,3 +45,12 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   return `${url}`
 }
 
+export const updateSearchParams = (type: string, value: string) =>  { 
+  const searchParams = new URLSearchParams(window.location.search)
+
+    searchParams.set(type, value)
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+    return newPathname
+  }
